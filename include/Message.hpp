6756bf2cda17cea60e3c150cd8662e3d8ac9dbf7
Dako Dimov <dako98@gmail.com>
2021-04-22 17:21:09 +0300

    Initial commit

---
+++ include/Message.hpp
@@ -0,0 +1,56 @@
+#ifndef __MESSAGE_H__
+#define __MESSAGE_H__
+
+#define debug
+
+#include <string>
+#include <memory>
+
+#include "User.hpp"
+
+using usrType = std::string;
+
+class Message
+{
+
+public:
+    usrType getSender() const;
+    usrType getReceiver() const;
+    std::string getContents() const;
+
+    Message()
+        : contents(""), receiver(""), sender("")
+    {
+    }
+
+    Message(const std::string &text,
+            const User &sender,
+            const User &receiver)
+        : receiver(receiver.getName()), sender(sender.getName()), contents(text)
+    {
+    }
+    Message(const std::string &text,
+            const std::string &sender,
+            const std::string &receiver)
+        : receiver(receiver), sender(sender), contents(text)
+    {
+    }
+    Message(const Message& other)
+    {
+        this->contents = other.contents;
+        this->receiver = other.receiver;
+        this->sender = other.sender;
+    }
+
+    friend std::ostream &operator<<(std::ostream &out, const Message &obj);
+
+    ~Message();
+
+private:
+
+    std::string contents;
+    usrType receiver;
+    usrType sender;
+};
+
+#endif // __MESSAGE_H__
---
+++ include/User.hpp
@@ -0,0 +1,53 @@
+#ifndef __USER_H__
+#define __USER_H__
+
+#define debug
+
+#include <string>
+#include <iostream>
+
+class User
+{
+
+public:
+    User()
+        : name(""), password("") {}
+    User(const std::string &name,
+         const std::string &password);
+
+    User(const User &other);
+
+    User &operator=(const User &other);
+    bool operator<(const User &other) const;
+    bool operator==(const User &other) const;
+
+    bool comparePassword(const std::string &string) const;
+
+    std::string getName() const;
+
+    ~User();
+
+    bool online;
+
+    void serialise() const;
+
+    friend std::ostream &operator<<(std::ostream &out, const User &obj);
+
+private:
+    std::string name;
+    std::string password;
+};
+namespace std
+{
+    template <>
+    struct hash<User>
+    {
+        size_t operator()(const User &x) const
+        {
+            return 1;
+//            return hash<std::string>()(x.getName());
+        }
+    };
+}
+
+#endif // __USER_H__
