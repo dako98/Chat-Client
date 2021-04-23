8b0d7d30a21500b97b70f939b4e2a64075ca1384
Dako Dimov <dako98@gmail.com>
2021-04-23 21:31:13 +0300

    Added menu and special request messages

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,21 @@
 #include <string>
 
 #include "Message.hpp"
+#include "MessageBuilder.hpp"
 
 using boost::asio::ip::tcp;
 
 const std::string ADDRESS("127.0.0.1");
 const std::string PORT("42123");
+
+std::string userName;
+std::string password;
+std::string receiver;
+std::string message;
+
 int sendMessage(tcp::socket &socket, const Message &message);
+int receiveMessage(tcp::socket &socket, Message &message);
+boost::system::error_code &messageReceiverV2(tcp::socket &socket, Message &message);
 
 enum Commands
 {
@@ -22,12 +31,17 @@ enum Commands
     COMMANDS_SIZE
 };
 
-int commandToCode(const std::string &command)
+static inline int commandToCode(const std::string &command)
 {
-    static std::array<std::string, COMMANDS_SIZE> commandsResolver = {"register", "login", "chat", "end"};
-    int code = INVALID;
+    static std::array<std::string, Commands::COMMANDS_SIZE> commandsResolver =
+        {"register",
+         "login",
+         "chat",
+         "end"};
 
-    for (int commandCode = 0; commandCode < COMMANDS_SIZE; ++commandCode)
+    int code = Commands::INVALID;
+
+    for (int commandCode = 0; commandCode < Commands::COMMANDS_SIZE; ++commandCode)
     {
         if (command == commandsResolver[commandCode])
         {
@@ -38,23 +52,262 @@ int commandToCode(const std::string &command)
     return code;
 }
 
+void requestHistory(tcp::socket &socket, const std::string &clientName, const std::string& participant)
+{
+    const std::string name = clientName, message = participant, receiver = "history";
+
+    MessageBuilder messageBuilder;
+
+    messageBuilder.setReceiver(receiver);
+    messageBuilder.setSender(name);
+    messageBuilder.setMessage(message);
+
+    Message request = messageBuilder.build();
+
+    sendMessage(socket, request);
+
+    receiveMessage(socket, request);
+
+    if (request.getSender() == "server")
+    {
+        if (request.getContents() == "")
+        {
+            std::cout << "(X) Getting history error. Server responded with empty response.";
+        }
+        else
+        {
+            int messages = atoi(request.getContents().c_str());
+
+            for (size_t i = 0; i < messages; i++)
+            {
+                receiveMessage(socket, request);
+                std::cout << request << '\n';
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Did not get response from server. Got \"" << request << "\" instead.";
+        throw std::invalid_argument("Invalid response.");
+    }
+}
+
+Message writeMessage(const std::string &clientName, const std::string &to)
+{
+    std::string text;
+    std::cout << "Message (max 255): ";
+
+    do
+    {
+        std::cin >> text;
+    } while (text.length() > MAX_SIZE);
+
+    MessageBuilder builder;
+    builder.setMessage(message);
+    builder.setReceiver(to);
+    builder.setSender(clientName);
+
+    return builder.build();
+}
+
+bool requestToChat(tcp::socket &socket, const std::string &clientName, const std::string &recipient)
+{
+    std::string name = clientName, message = recipient, receiver = "server";
+    bool valid = true;
+
+    MessageBuilder messageBuilder;
+
+    valid &= messageBuilder.setSender(name);
+    valid &= messageBuilder.setMessage(message);
+
+    messageBuilder.setReceiver(receiver);
+
+    Message request = messageBuilder.build();
+
+    if (valid)
+    {
+        sendMessage(socket, request);
+
+        receiveMessage(socket, request);
+    }
+
+    if (request.getSender() == "server")
+    {
+        if (request.getContents() != "")
+        {
+            std::cout << "(X) Login error. Server responded with: " << request.getContents() << '\n';
+            valid = false;
+        }
+        else
+        {
+            valid = true;
+        }
+    }
+    else
+    {
+        std::cout << "Did not get response from server. Got \"" << login << "\" instead.";
+        throw std::invalid_argument("Invalid response.");
+    }
+    return valid;
+}
+
+void chat(tcp::socket &socket, const std::string &clientName)
+{
+    std::string recipient;
+
+    std::cout << "Chat with: ";
+    std::cin >> recipient;
+    bool allowedToChat = requestToChat(socket, clientName, recipient);
+
+    if (allowedToChat)
+    {
+        // receive chat history
+        requestHistory(socket, clientName, recipient);
+    }
+
+    Message sessionMessage;
+    while (allowedToChat)
+    {
+        // receive session message
+        receiveMessage(socket, sessionMessage);
+
+        // display message
+        std::cout << sessionMessage << '\n';
+
+        // write message
+        sessionMessage = writeMessage(clientName, recipient);
+
+        if (sessionMessage.getContents() == "end")
+        {
+            allowedToChat = false;
+            break;
+        }
+
+        // send message
+        sendMessage(socket, sessionMessage);
+    } // send/receive loop
+}
+
+bool login(tcp::socket &socket, std::string &loggedinName)
+{
+    std::string name, password, receiver = "server";
+    bool valid = true, successfullLogin;
+
+    MessageBuilder messageBuilder;
+    do
+    {
+        std::cout << "Ender name: ";
+        valid &= messageBuilder.setSender(name);
+        std::cout << "Ender password: ";
+        valid &= messageBuilder.setMessage(password);
+
+    } while (!valid);
+
+    messageBuilder.setReceiver(receiver);
+
+    Message login = messageBuilder.build();
+
+    sendMessage(socket, login);
+
+    receiveMessage(socket, login);
+
+    if (login.getSender() == "server")
+    {
+        if (login.getContents() != "")
+        {
+            std::cout << "(X) Login error. Server responded with: " << login.getContents() << '\n';
+            successfullLogin = false;
+        }
+        else
+        {
+            userName = loggedinName;
+            successfullLogin = true;
+        }
+    }
+    else
+    {
+        std::cout << "Did not get response from server. Got \"" << login << "\" instead.";
+        throw std::invalid_argument("Invalid response.");
+    }
+    return successfullLogin;
+}
+
+bool registerUser(tcp::socket &socket, std::string &registeredName)
+{
+    std::string name, password, receiver = "register";
+    bool valid = true, successfullRegistration;
+
+    MessageBuilder messageBuilder;
+    do
+    {
+        std::cout << "Ender name (max 255): ";
+        valid &= messageBuilder.setSender(name);
+        std::cout << "Ender password (max 255): ";
+        valid &= messageBuilder.setMessage(password);
+
+    } while (!valid);
+
+    messageBuilder.setReceiver(receiver);
+
+    Message registration = messageBuilder.build();
+
+    sendMessage(socket, registration);
+
+    receiveMessage(socket, registration);
+
+    if (registration.getSender() == "server")
+    {
+        if (registration.getContents() != "")
+        {
+            std::cout << "(X) Registration error. Server responded with: " << registration.getContents() << '\n';
+            successfullRegistration = false;
+        }
+        else
+        {
+            userName = registeredName;
+            successfullRegistration = true;
+        }
+    }
+    else
+    {
+        std::cout << "Did not get response from server. Got \"" << registration << "\" instead.";
+        throw std::invalid_argument("Invalid response.");
+    }
+    return successfullRegistration;
+}
+
 void menu(tcp::socket &&socket)
 {
     // No authentication. Out of scope.
 
     // Send authentication message
-    Message initialMessage = Message(std::string("pass1"), std::string("client1"), std::string("server"));
+    /*    Message initialMessage = Message(std::string("pass2"), std::string("client2"), std::string("server"));
     sendMessage(socket, initialMessage);
-
-// TODO: for testing purpouses only:
-    Message subsequent = Message("hello", "client1", "client2");
+*/
+    // TODO: for testing purpouses only:
+    /*    Message subsequent = Message("hello", "client1", "client2");
     sendMessage(socket, subsequent);
 
     subsequent = Message("loopback", "client1", "client1");
     sendMessage(socket, subsequent);
+*/
+
+    /*    Message initialMessage = Message(password, userName, receiver);
+    sendMessage(socket, initialMessage);
+
+    initialMessage = Message(message, userName, receiver);
+    sendMessage(socket, initialMessage);
+
+    Message receivedMessage;
+    receiveMessage(socket, receivedMessage);
 
 
-    // While not quit chatting
+#ifdef debug
+    std::cout << receivedMessage << '\n';
+#endif
+*/
+
+    // While invalid
     std::string command;
     int commandCode;
     do
@@ -64,21 +317,40 @@ void menu(tcp::socket &&socket)
 
         switch (commandCode)
         {
-        case CHAT:
+        case Commands::REGISTER:
+            bool registrationStatus = registerUser(socket, userName);
+            if (registrationStatus)
+            {
+                std::cout << "Registered. Type: login <name> <password>.";
+            }
 
             break;
-        case END:
+        case Commands::LOGIN:
+            bool loginStatus = login(socket, userName);
+            if (loginStatus)
+            {
+                chat(socket, userName);
+            }
 
+            break;
+        case Commands::CHAT:
+            // TODO: Remove
+            break;
+        case Commands::END:
+            std::cout << "Invalid command. You are not in a chat.\n";
+
+            break;
+        case Commands::INVALID:
+            std::cout << "Invalid command.\n";
             break;
 
         default:
             break;
         }
 
-    } while (commandCode != END);
+    } while (commandCode != Commands::END);
 
     std::this_thread::sleep_for(std::chrono::seconds(100));
-
 }
 
 int sendMessage(tcp::socket &socket, const Message &message)
@@ -99,8 +371,28 @@ int sendMessage(tcp::socket &socket, const Message &message)
     return code;
 }
 
+int receiveMessage(tcp::socket &socket, Message &message)
+{
+    messageReceiverV2(socket, message);
+}
+
+boost::system::error_code &messageReceiverV2(tcp::socket &socket, Message &message)
+{
+    boost::system::error_code error;
+
+    MessageBuilder receivedMessage;
+    error = receivedMessage.setAll(socket);
+    if (error)
+        throw std::exception();
+
+    message = receivedMessage.build();
+    return error;
+}
+
 int main()
 {
+    std::cout << "Username, password, receiver, message\n";
+    std::cin >> userName >> password >> receiver >> message;
 
     try
     {
